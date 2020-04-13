2eec7dcab12464ae2d78674b83338abc9d43015d
Home <Home@Emdy-PC>
2020-04-13 20:57:01 +0200

    clase añadida 1304

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author María Dolores Romero Martínez
+ * @version 23/03/20 v.1
+ */
+public class Calculadora {
+    private int num1;
+    private int num2;
+    
+    /**
+     * Constructor 
+     * @param a
+     * @param b 
+     */
+    public Calculadora (int a, int b){
+        num1=a;
+        num2=b;
+                
+    }
+    
+    
+    /**
+     * Método suma, realiza suma las variables de la clase
+     * @return resultado
+     */
+    public int suma(){
+        int result=num1+num2;
+        return result;
+    }
+    
+    /**
+     * Método resta, realiza la resta de las variables de la clase
+     * @return resultado
+     */
+    public int resta(){
+        int result=num1-num2;
+        return result;
+    }
+    
+    /**
+     * Método multiplica, realiza el producto de las variables de la clase
+     * @return resultado
+     */
+    public int multiplica(){
+        int result=num1*num2;
+        return result;
+    }
+    
+    /**
+     * Método divide, realiza la división entre las variables de la clase
+     * @return resultado
+     * @throws ArithmeticException divisón por 0
+     */
+    public int divide(){
+        if (num2==0){
+            throw new java.lang.ArithmeticException("División por 0");
+        }else{
+        int result=num1/num2;
+        return result;
+        }
+    }
+}
